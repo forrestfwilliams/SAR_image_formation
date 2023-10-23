27d5ee81e4e0d27861a45c3a3a1753858b4e2d95
Forrest Williams <ffwilliams2@alaska.edu>
2023-10-23 17:15:20 -0500

    tried to make taylor young work

--- src/sar_course/hw1.py
+++ src/sar_course/hw1.py
@@ -1,8 +1,32 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 FOURTH_PI = -11
 BOLTZMAN = 1.38e-23
 SPEED_OF_LIGHT = 2.998e8
+
+
+def taylor_young_step(t, frequency, distance, boat_start=1000, speed=5):
+    reciever_travel_time = distance / SPEED_OF_LIGHT
+    reciever_value = np.cos(2 * np.pi * frequency * (t + reciever_travel_time))
+    boat_distance = 2 * np.sqrt(((distance / 2) ** 2) + ((boat_start - (speed * t)) ** 2))
+    boat_time = boat_distance / SPEED_OF_LIGHT
+    boat_value = -0.5 * np.cos(2 * np.pi * frequency * (t + boat_time))
+    return reciever_value + boat_value
+
+
+def taylor_young(wavelength=5, distance=5000, show=False):
+    frequency = SPEED_OF_LIGHT / wavelength
+    steps = np.linspace(0, 1e-7, 1000)
+    interference_values = [taylor_young_step(t, frequency, distance) for t in steps]
+    f, ax = plt.subplots(1, 1, figsize=(6, 6))
+    ax.plot(np.array(steps), interference_values)
+    ax.set(xlabel='Time (s)', ylabel='Amplitude')
+    if show:
+        plt.show()
+    else:
+        plt.savefig('../../assets/taylor_young.png')
+    return interference_values
 
 
 def to_db(value):
@@ -52,17 +76,19 @@ def get_snr_continuous(
         ]
     )
     noise_components = [to_db(x) for x in [noise_temp, noise_bandwidth, BOLTZMAN]]
-
+    print(np.array(signal_components).sum(), np.array(noise_components).sum())
     return np.array(signal_components).sum() - np.array(noise_components).sum()
 
 
-if __name__ == '__main__':
+def question2():
     base_case = get_snr_continuous(2500, 1, 0.5, 0.5, 0.24, 15_000, 50, -15, 900, 1_000_000)
     print(f'Base Case: {base_case:.2f} dB')
 
     new_wavelength_case = get_snr_continuous(2500, 1, 0.5, 0.5, 0.03, 15_000, 50, -15, 900, 1_000_000)
     print(f'3 cm Case: {new_wavelength_case:.2f} dB')
 
+
+def question3():
     # Fixed
     altitude = 8000
     grazing_angle = 45
@@ -90,3 +116,9 @@ if __name__ == '__main__':
     distance = altitude_to_distance(altitude, grazing_angle)
     design1 = get_snr_continuous(power, 1, 0.5, 2, wavelength, distance, area, -15, 900, 1_000_000)
     print(f'K-band Design: {design1:.2f} dB')
+
+
+if __name__ == '__main__':
+    taylor_young(show=True)
+    question2()
+    question3()
